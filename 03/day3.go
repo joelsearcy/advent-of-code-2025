68af6a4332c54bf8db5c47638f53bc82d41663e4
joelsearcy <searcy.joel@gmail.com>
2025-12-04 14:14:08 -0700

    day 3 solutions

---
+++ 03/day3.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	data, err := os.ReadFile("input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	// split the input data into lines
+	lines := strings.Split(strings.TrimSpace(string(data)), "\n")
+	totalJoltage := 0
+	batteryBankSize := 12
+
+	maxJoltage := make([]byte, 0, batteryBankSize)
+	// find the ordered "set" of digits (not necessarily adjacent) that make the largest N-digit number possible in each line
+	for _, line := range lines {
+		maxJoltage = maxJoltage[:0]
+		startIndex := 0
+		for i := range batteryBankSize {
+			endIndex := len(line) - (batteryBankSize - i) + 1
+			if startIndex >= endIndex {
+				break
+			}
+			maxDigitIndex, maxDigit := findIndexOfNextMaxDigit(line[:endIndex], startIndex)
+			maxJoltage = append(maxJoltage, maxDigit)
+			startIndex = maxDigitIndex + 1
+		}
+		joltage, err := strconv.Atoi(string(maxJoltage))
+		if err != nil {
+			panic(err)
+		}
+		totalJoltage += joltage
+	}
+
+	fmt.Printf("Total Joltage: %d\n", totalJoltage)
+}
+
+func findIndexOfNextMaxDigit(line string, startIndex int) (int, byte) {
+	maxDigit := line[startIndex]
+	maxDigitIndex := startIndex
+	for i := startIndex + 1; i < len(line); i++ {
+		if line[i] > maxDigit {
+			maxDigit = line[i]
+			maxDigitIndex = i
+		}
+	}
+	return maxDigitIndex, maxDigit
+}
